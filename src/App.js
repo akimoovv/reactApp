2b5ffe6cf879f560381ecdebb08fb6eb994ce578
Ivan Akimov <akimoovv@MacBook-Air-Ivan.local>
2021-05-19 00:45:45 +0300

    m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import './toDO/ToDoList'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"App"}>
+          <Header/>
+         <ul>
+             <li>1</li>
+             <li>2</li>
+             <li>3</li>
+         </ul>
+      </div>
   );
-}
+};
+
+const Header = () => {
+
+    return (
+    <div>
+        <h1>From header</h1>
+    </div>);
+};
+
 
 export default App;
---
+++ src/toDO/todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ToDoList() {
+    return (
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+        </ul>
+    )
+}
